3c5bea44cc292eec8319644e62597815b0333996
William <dpwilliamj@gmail.com>
2018-01-29 14:14:13 +1030

    inital commit
    adding all code, and compiled .exe

---
+++ WFA_ArduinoSerial_001/WFA_ArduinoSerial_001/Form1.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.comPort_ComboBox = New System.Windows.Forms.ComboBox()
+        Me.COMport_LBL = New System.Windows.Forms.Label()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.clear_BTN = New System.Windows.Forms.Button()
+        Me.connect_BTN = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer_LBL = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'comPort_ComboBox
+        '
+        Me.comPort_ComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.comPort_ComboBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.comPort_ComboBox.FormattingEnabled = True
+        Me.comPort_ComboBox.Location = New System.Drawing.Point(34, 66)
+        Me.comPort_ComboBox.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.comPort_ComboBox.Name = "comPort_ComboBox"
+        Me.comPort_ComboBox.Size = New System.Drawing.Size(180, 37)
+        Me.comPort_ComboBox.TabIndex = 18
+        '
+        'COMport_LBL
+        '
+        Me.COMport_LBL.AutoSize = True
+        Me.COMport_LBL.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.COMport_LBL.Location = New System.Drawing.Point(63, 31)
+        Me.COMport_LBL.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.COMport_LBL.Name = "COMport_LBL"
+        Me.COMport_LBL.Size = New System.Drawing.Size(119, 29)
+        Me.COMport_LBL.TabIndex = 17
+        Me.COMport_LBL.Text = "COM Port"
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.DtrEnable = True
+        Me.SerialPort1.RtsEnable = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(444, 66)
+        Me.RichTextBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(483, 473)
+        Me.RichTextBox1.TabIndex = 23
+        Me.RichTextBox1.Text = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(504, 25)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 29)
+        Me.Label1.TabIndex = 22
+        Me.Label1.Text = "INPUT"
+        '
+        'clear_BTN
+        '
+        Me.clear_BTN.Location = New System.Drawing.Point(494, 551)
+        Me.clear_BTN.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.clear_BTN.Name = "clear_BTN"
+        Me.clear_BTN.Size = New System.Drawing.Size(112, 35)
+        Me.clear_BTN.TabIndex = 21
+        Me.clear_BTN.Text = "CLEAR"
+        Me.clear_BTN.UseVisualStyleBackColor = True
+        '
+        'connect_BTN
+        '
+        Me.connect_BTN.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.connect_BTN.Location = New System.Drawing.Point(225, 66)
+        Me.connect_BTN.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.connect_BTN.Name = "connect_BTN"
+        Me.connect_BTN.Size = New System.Drawing.Size(180, 49)
+        Me.connect_BTN.TabIndex = 19
+        Me.connect_BTN.Text = "Connect"
+        Me.connect_BTN.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 500
+        '
+        'Timer_LBL
+        '
+        Me.Timer_LBL.AutoSize = True
+        Me.Timer_LBL.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Timer_LBL.Location = New System.Drawing.Point(28, 152)
+        Me.Timer_LBL.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Timer_LBL.Name = "Timer_LBL"
+        Me.Timer_LBL.Size = New System.Drawing.Size(138, 29)
+        Me.Timer_LBL.TabIndex = 24
+        Me.Timer_LBL.Text = "Timer: OFF"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(988, 618)
+        Me.Controls.Add(Me.Timer_LBL)
+        Me.Controls.Add(Me.comPort_ComboBox)
+        Me.Controls.Add(Me.COMport_LBL)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.clear_BTN)
+        Me.Controls.Add(Me.connect_BTN)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Form1"
+        Me.Text = "TotoEV RFID Demo"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents comPort_ComboBox As System.Windows.Forms.ComboBox
+    Friend WithEvents COMport_LBL As System.Windows.Forms.Label
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents clear_BTN As System.Windows.Forms.Button
+    Friend WithEvents connect_BTN As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Timer_LBL As System.Windows.Forms.Label
+
+End Class
---
+++ WFA_ArduinoSerial_001/WFA_ArduinoSerial_001/Form1.vb
@@ -0,0 +1,155 @@
+﻿' Arduino and Visual Basic: Receiving Data From the Arduino
+' A simple example of recing data from an Arduino
+'
+
+Imports System.IO
+Imports System.IO.Ports
+Imports System.Net
+Imports System.Text
+
+Public Class Form1
+
+    Dim comPORT As String
+    Dim receivedData As String = ""
+    Dim Card1Charging As String = "Stopped"
+    Dim Card2Charging As String = "Stopped"
+    Dim CardIDNumber As Integer
+    Dim AccessToken As String = "174303fb4870ed9400655d3f086ee990"
+
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls Or SecurityProtocolType.Tls11 Or SecurityProtocolType.Tls12
+        Timer1.Enabled = False
+        comPORT = ""
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            comPort_ComboBox.Items.Add(sp)
+        Next
+    End Sub
+
+
+    Private Sub comPort_ComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles comPort_ComboBox.SelectedIndexChanged
+        If (comPort_ComboBox.SelectedItem <> "") Then
+            comPORT = comPort_ComboBox.SelectedItem
+        End If
+    End Sub
+
+
+    Private Sub connect_BTN_Click(sender As Object, e As EventArgs) Handles connect_BTN.Click
+        If (connect_BTN.Text = "Connect") Then
+            If (comPORT <> "") Then
+                SerialPort1.Close()
+                SerialPort1.PortName = comPORT
+                SerialPort1.BaudRate = 9600
+                SerialPort1.DataBits = 8
+                SerialPort1.Parity = Parity.None
+                SerialPort1.StopBits = StopBits.One
+                SerialPort1.Handshake = Handshake.None
+                SerialPort1.Encoding = System.Text.Encoding.Default 'very important!
+                SerialPort1.ReadTimeout = 10000
+
+                SerialPort1.Open()
+                connect_BTN.Text = "Dis-connect"
+                Timer1.Enabled = True
+                Timer_LBL.Text = "Timer: ON"
+            Else
+                MsgBox("Select a COM port first")
+            End If
+        Else
+            SerialPort1.Close()
+            connect_BTN.Text = "Connect"
+            Timer1.Enabled = False
+            Timer_LBL.Text = "Timer: OFF"
+        End If
+
+
+    End Sub
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        receivedData = ReceiveSerialData()
+        RichTextBox1.Text &= receivedData
+
+        If receivedData.Contains("Network: NRMA") And receivedData.Contains("Card ID: 8RPCI-4QKIL-J1DC") Then
+            If Card1Charging = "Stopped" Then
+                Card1Charging = "Started"
+            ElseIf Card1Charging = "Started" Then
+                Card1Charging = "Stopped"
+            End If
+            CardIDNumber = 3
+            API(ChargingState:=Card1Charging, CardID:="8RPCI-4QKIL-J1DC", Network:="NRMA")
+        ElseIf receivedData.Contains("Network: ActewAGL") And receivedData.Contains("Card ID: 6L69O-12D68-3N3L") Then
+            If Card2Charging = "Stopped" Then
+                Card2Charging = "Started"
+            ElseIf Card2Charging = "Started" Then
+                Card2Charging = "Stopped"
+            End If
+            CardIDNumber = 2
+            API(ChargingState:=Card2Charging, CardID:="6L69O-12D68-3N3L", Network:="ActewAGL")
+        End If
+
+
+
+    End Sub
+
+
+    Function ReceiveSerialData() As String
+        Dim Incoming As String
+        Try
+            Incoming = SerialPort1.ReadExisting()
+            If Incoming Is Nothing Then
+                Return "nothing" & vbCrLf
+            Else
+                Return Incoming
+            End If
+        Catch ex As TimeoutException
+            Return "Error: Serial Port read timed out."
+        End Try
+
+    End Function
+
+
+
+    Private Sub clear_BTN_Click(sender As Object, e As EventArgs) Handles clear_BTN.Click
+        RichTextBox1.Text = ""
+    End Sub
+
+    Public Function API(ChargingState As String, CardID As String, Network As String) As String
+        Dim BodyText As String = Nothing
+        Dim InUse As Integer = 0
+        MsgBox(ChargingState)
+        If ChargingState = "Stopped" Then
+            InUse = 0
+        ElseIf ChargingState = "Started" Then
+            InUse = 1
+        End If
+
+        BodyText = "{""in_use"":" & InUse & ",""assigned_to"": 4}"
+        MsgBox(BodyText)
+        'convert text into ascii
+        Dim encoding As New ASCIIEncoding()
+        Dim Bytes As Byte() = encoding.GetBytes(BodyText)
+        Dim request = TryCast(System.Net.WebRequest.Create("https://server.totoev.com.au/api/cards/" & CardIDNumber), System.Net.HttpWebRequest)
+
+        request.Method = "PUT"
+        request.Headers.Add("X-Access-Token", AccessToken)
+        request.ContentType = "application/json"
+        request.ContentLength = Bytes.Length
+
+        'write the info into the json body
+        Dim Stream = request.GetRequestStream()
+        Stream.Write(Bytes, 0, Bytes.Length)
+        Stream.Close()
+
+
+        Dim responseContent As String
+        Using response = TryCast(request.GetResponse(), System.Net.HttpWebResponse)
+            Using reader = New System.IO.StreamReader(response.GetResponseStream())
+                responseContent = reader.ReadToEnd()
+                MessageBox.Show(responseContent)
+            End Using
+        End Using
+        Return True
+    End Function
+
+End Class
